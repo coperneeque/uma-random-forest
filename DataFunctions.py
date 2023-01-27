aea9b6f34d1aeb0a93a98dc749a1d0a3d116f6a5
Mikołaj Pańka <01149377@pw.edu.pl>
2023-01-27 15:41:39 +0100

    usuwanie brakujacych dyskretnuch danych

--- DataFunctions.py
+++ DataFunctions.py
@@ -22,7 +22,7 @@ def str_column_to_float(dataset, column):
 			# print()
 			row[column] = nan
 
-def clean_nans(dataset):
+def clean_nans_float(dataset):
 	"""Zastępuje brakujące wartości atrybutów medianami"""
 	num_nans = 0
 	num_attributes = len(dataset[0]) - 1  # ostatni to klasa
@@ -39,6 +39,28 @@ def clean_nans(dataset):
 			if row[i] is nan:
 				num_nans += 1
 				row[i] = medians[i]
+	return num_nans
+
+def clean_nans_labels(dataset):
+	"""Usuwa przykłady z brakującymi wartościami atrybutów"""
+	num_nans = 0
+	num_attributes = len(dataset[0]) - 1  # ostatni to klasa
+	# rows_to_del = []
+	# print("Liczba atybutów %s" %num_attributes)
+		# row_idx = 0
+		# while row_idx < len(dataset):
+		# 	row_idx += 1
+	for row_idx in reversed(range(len(dataset))):
+	# for row_idx in range(len(dataset)):
+		for i in range(num_attributes):
+			if dataset[row_idx][i] == "":
+				# print(dataset[row_idx])
+				# print(f"Długość przykładu (bez klasy):\n{len(dataset[row_idx]) - 1}")
+				del dataset[row_idx]
+				# rows_to_del.append(row_idx)
+				num_nans += 1
+				# row_idx -= 1  # trzeba powrócić do tego wiersza, bo po usunięciu następny się przesunął tutaj, gdzie ejsteśmy
+	# del dataset[rows_to_del]
 	return num_nans
 
 def str_column_to_int(dataset, column):
--- UMArandomForest.py
+++ UMArandomForest.py
@@ -17,18 +17,21 @@ if __name__ == "__main__":
 		for i in range(0, len(dataset[0])-1):
 			df.str_column_to_float(dataset, i)
 		df.str_column_to_int(dataset, len(dataset[0])-1)
+		num_nans = df.clean_nans_float(dataset)
+		print(f"Uzupełniono {num_nans} brakujących wartości.")
 	elif datasetType!="discrete":
 		sys.exit(1)
-	num_nans = df.clean_nans(dataset)
-	print(f"Uzupełniono {num_nans} brakujących wartości.")
+	if datasetType == "discrete":
+		num_nans = df.clean_nans_labels(dataset)
+		print(f"Usunięto {num_nans} przykładów z brakującymi wartościami.")
 	# sys.exit()
 	n_folds = 5
 	max_depth = 7
 	min_size = 1
 	feature_size = int(2*sqrt(len(dataset[0])-1))
 	tournament_size = 0
-	for n_trees in [5,15,25,50]:
-	# for n_trees in [5]:
+	# for n_trees in [5,15,25,50]:
+	for n_trees in [5]:
 		scores = e.evaluate_algorithm(dataset, rf.random_forest, n_folds, max_depth, min_size, n_trees, feature_size, tournament_size)
 		print('Liczba drzew w lesie: %d' % n_trees)
 		print('Poprawnosc prdykcji w kolejnych iteracjach: %s' % scores)
